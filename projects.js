b01af2f5a043d7873532211fea63ebdbcbf0e509
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-04-11 11:35:22 -0400

    for loops

--- projects.js
+++ projects.js
@@ -13,3 +13,19 @@ var vacationSpots = ['miami', 'san', 'dallas'];
 for (var i = vacationSpots.length - 1; i >= 0; i--) {
   console.log('I would love to visit ' + vacationSpots[i]);
 }
+
+
+
+
+//for loop//
+var myPlaces = ['Orlando', 'New York City', 'San Francisco'];
+
+var friendPlaces = ['New York City', 'Boston', 'Dallas'];
+
+for (var i = 0; i < myPlaces.length; i++) {
+  for (var j = 0; j < friendPlaces.length; j++) {
+    if (myPlaces[i] === friendPlaces[j]) {
+    console.log('Match: ' + myPlaces[i]);     
+  }
+}
+}
